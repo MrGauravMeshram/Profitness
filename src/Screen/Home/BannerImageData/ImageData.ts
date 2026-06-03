9e2621c66def2d504dfd60db4fa5e9259ca57857
MrGauravMeshram <meshramgaurav482@gmail.com>
2026-06-03 09:47:55 +0530

    jdf

---
+++ src/Screen/Home/BannerImageData/ImageData.ts
@@ -0,0 +1,14 @@
+export const BannerData = [
+  {
+    id: '1',
+    image: require('../../../assets/png/fitness.png'),
+  },
+  {
+    id: '2',
+    image: require('../../../assets/png/Perfectbody.png'),
+  },
+  {
+    id: '3',
+    image: require('../../../assets/png/BeStrong.png'),
+  },
+];
