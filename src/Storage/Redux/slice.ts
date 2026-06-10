029033dd9ab3385f647db865d6cfeb285fad3a3f
MrGauravMeshram <meshramgaurav482@gmail.com>
2026-06-10 10:07:21 +0530

    dfiojasd p

---
+++ src/Storage/Redux/slice.ts
@@ -0,0 +1,74 @@
+// redux/slices/userSlice.ts
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface UserDetails {
+  userName: string;
+  userNumber: string;
+  userEmail: string;
+  userWeight: string;
+  userHeight: string;
+  userGender: string;
+  userAge: string;
+  userWeightUnit: string;
+  userHeightUnit: string;
+}
+
+interface UserState {
+  uid: string | null;
+  name: string | null;
+  email: string | null;
+  photoURL: string | null;
+  userDetails: UserDetails | null;
+  profileImage: string | null;
+}
+
+const initialState: UserState = {
+  uid: null,
+  name: null,
+  email: null,
+  photoURL: null,
+  userDetails: null,
+  profileImage: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (
+      state,
+      action: PayloadAction<{
+        uid: string | null;
+        name: string | null;
+        email: string | null;
+        photoURL: string | null;
+      }>
+    ) => {
+      state.uid = action.payload.uid;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.photoURL = action.payload.photoURL;
+    },
+
+    setUserDetails: (state, action: PayloadAction<UserDetails | null>) => {
+      state.userDetails = action.payload;
+    },
+
+    setProfileImage: (state, action: PayloadAction<string | null>) => {
+      state.profileImage = action.payload;
+    },
+
+    clearUser: state => {
+      state.uid = null;
+      state.name = null;
+      state.email = null;
+      state.photoURL = null;
+      state.userDetails = null;
+      state.profileImage = null;
+    },
+  },
+});
+
+export const { setUser, setUserDetails, setProfileImage, clearUser } = userSlice.actions;
+export default userSlice.reducer;
---
+++ src/Storage/Redux/store.ts
@@ -0,0 +1,9 @@
+import userReducer from './slice'
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {userReducer},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
